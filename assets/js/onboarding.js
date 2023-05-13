34e8263d1f81701f3b1048bee852c32419770dc7
MrDanielHarka <daniel@harka.com>
2023-05-13 16:39:20 +0200

    Add dynamic question eventlisteners.

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -1,54 +1,38 @@
+const questionElements = document.getElementsByClassName('onboarding-question');
+
+const createQuerySelectorForQuestion = currentQuestion => {
+  document
+    .querySelector(`.button-onboarding-${currentQuestion}`)
+    .addEventListener('click', () => {
+      document.querySelector(
+        `.onboarding-question-${currentQuestion}`
+      ).style.display = 'none';
+      document.querySelector(
+        `.onboarding-question-${currentQuestion + 1}`
+      ).style.display = 'block';
+    });
+};
+
+const addEventListenerToAllQuestionElements = () => {
+  for (let i = 1; i < questionElements.length + 1; i++) {
+    if (i === questionElements.length) {
+      document
+        .querySelector(`.button-onboarding-${questionElements.length}`)
+        .addEventListener('click', () => {
+          document.querySelector(
+            `.onboarding-question-${questionElements.length}`
+          ).innerHTML =
+            'Thanks for completing the onboarding process! <br> <br> You´ll receive an email with your login details soon. <br> <br> <a href="index.html">Go to the homepage</a>';
+        });
+    } else {
+      createQuerySelectorForQuestion(i);
+    }
+  }
+};
+
+addEventListenerToAllQuestionElements();
+
 document.querySelector('.button-sign-up').addEventListener('click', () => {
   document.querySelector('.onboarding').style.display = 'none';
   document.querySelector('.onboarding-question-1').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-1').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-1').style.display = 'none';
-  document.querySelector('.onboarding-question-2').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-2').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-2').style.display = 'none';
-  document.querySelector('.onboarding-question-3').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-3').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-3').style.display = 'none';
-  document.querySelector('.onboarding-question-4').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-4').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-4').style.display = 'none';
-  document.querySelector('.onboarding-question-5').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-5').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-5').style.display = 'none';
-  document.querySelector('.onboarding-question-6').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-6').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-6').style.display = 'none';
-  document.querySelector('.onboarding-question-7').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-7').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-7').style.display = 'none';
-  document.querySelector('.onboarding-question-8').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-8').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-8').style.display = 'none';
-  document.querySelector('.onboarding-question-9').style.display = 'block';
-});
-
-document.querySelector('.button-onboarding-9').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-9').style.display = 'none';
-  document.querySelector('.onboarding-question-10').style.display = 'block';
-});
-
-
-document.querySelector('.button-onboarding-10').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-10').innerHTML = 'Thanks for completing the onboarding process! <br> <br> You´ll receive an email with your login details soon. <br> <br> <a href="index.html">Go to the homepage</a>';
 });
