dc0bf6bb6b35351df8975f0d1ba7872a48f0ebd2
chiara <chiaramori66@gmail.com>
2023-05-13 16:25:02 +0200

    Styled contenct homepage

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -24,5 +24,31 @@ document.querySelector('.button-onboarding-4').addEventListener('click', () => {
 });
 
 document.querySelector('.button-onboarding-5').addEventListener('click', () => {
-  document.querySelector('.onboarding-question-5').innerHTML = 'Thanks!';
+  document.querySelector('.onboarding-question-5').style.display = 'none';
+  document.querySelector('.onboarding-question-6').style.display = 'block';
+});
+
+document.querySelector('.button-onboarding-6').addEventListener('click', () => {
+  document.querySelector('.onboarding-question-6').style.display = 'none';
+  document.querySelector('.onboarding-question-7').style.display = 'block';
+});
+
+document.querySelector('.button-onboarding-7').addEventListener('click', () => {
+  document.querySelector('.onboarding-question-7').style.display = 'none';
+  document.querySelector('.onboarding-question-8').style.display = 'block';
+});
+
+document.querySelector('.button-onboarding-8').addEventListener('click', () => {
+  document.querySelector('.onboarding-question-8').style.display = 'none';
+  document.querySelector('.onboarding-question-9').style.display = 'block';
+});
+
+document.querySelector('.button-onboarding-9').addEventListener('click', () => {
+  document.querySelector('.onboarding-question-9').style.display = 'none';
+  document.querySelector('.onboarding-question-10').style.display = 'block';
+});
+
+
+document.querySelector('.button-onboarding-10').addEventListener('click', () => {
+  document.querySelector('.onboarding-question-10').innerHTML = 'Thanks for completing the onboarding process! <br> <br> You´ll receive an email with your login details soon. <br> <br> <a href="index.html">Go to the homepage</a>';
 });
